62a1b77e8a70d4c5fd6aa537d48be68623334e1e
Quentin Lévêque <qleveque21@gmail.com>
2026-04-12 11:07:50 +0200

    feat: make backend proxy URL configurable at runtime

--- frontend/core/docker-entrypoint.sh
+++ frontend/core/docker-entrypoint.sh
@@ -9,4 +9,9 @@ window.__config = {
 };
 EOF
 
+BACKEND_URL="${BACKEND_URL:-http://backend:8081}" \
+  envsubst '${BACKEND_URL}' \
+  < /etc/nginx/conf.d/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
 exec nginx -g "daemon off;"
